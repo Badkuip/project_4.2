a5c7aa9cca2075ecbd8e9698c805f85ce78360f1
B.Kuiper <46485306+Badkuip@users.noreply.github.com>
2021-12-15 09:47:31 +0100

    Create dc_home_dir_sync_rollout.sh.
    Create dc_home_dir_sync_rollout.sh.

---
+++ dc_home_dir_sync_rollout.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+touch /srv/sync_script.sh
+tee -a /srv/sync_script.sh <<EOF
+#!/bin/bash
+amount_of_home_folders=$(ls -l /home/ | grep ^d | wc -l)
+amount_of_nfs_folders=$(ls -l /srv/ldap-home/ | grep ^d | wc -l)
+
+if [ $amount_of_home_folders != $amount_of_nfs_folders ]; then
+  for d in /home/*; do
+    if [ ! -d "/srv/ldap-home/$(basename $d)@groep5.local" ]; then
+      cp -r "$d" "/srv/ldap-home/$(basename $d)@groep5.local"
+    fi
+  done
+fi
+EOF
+
+mkdir /srv/nfs-home
+echo '10.15.1.13:/srv/ldap-home /srv/nfs-home nfs defaults 0 0'  >> /etc/fstab
+echo "* * * * * /srv/sync_script.sh >/dev/null 2>&1" >> /etc/crontab
+mount 10.15.1.13:/srv/ldap-home /srv/nfs-home
