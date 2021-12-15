8d24635c0b9681f619187db730d51116e850aad3
B.Kuiper <46485306+Badkuip@users.noreply.github.com>
2021-12-15 10:43:03 +0100

    Fixed bug.
    Fixed bug.

--- dc_home_dir_sync_rollout.sh
+++ dc_home_dir_sync_rollout.sh
@@ -11,8 +11,8 @@ tee -a /srv/sync_script.sh <<EOF
 
 'if [ $amount_of_home_folders != $amount_of_nfs_folders ]; then'
   'for d in /home/*; do'
-    'if [ ! -d "/srv/ldap-home/$(basename $d)@groep5.local" ]; then'
-      'cp -r "$d" "/srv/ldap-home/$(basename $d)@groep5.local"'
+    'if [ ! -d "/srv/nfs-home/$(basename $d)@groep5.local" ]; then'
+      'cp -r "$d" "/srv/nfs-home/$(basename $d)@groep5.local"'
     'fi'
   'done'
 'fi'
