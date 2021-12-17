17de19172107c2e710730eca398f5caa4ba42705
B.Kuiper <46485306+Badkuip@users.noreply.github.com>
2021-12-17 10:07:12 +0100

    Fixed bug.
    Fixed bug.

--- undo_echo_ubuntu.sh
+++ undo_echo_ubuntu.sh
@@ -3,4 +3,4 @@ sed -i '/http_proxy/d' /etc/apt/apt.conf.d/proxy.conf
 sed -i '/https_proxy/d' /etc/apt/apt.conf.d/proxy.conf
 sed -i '/http_proxy/d' /etc/environment
 sed -i '/https_proxy/d' /etc/environment
-sed -i '*.* @@10.15.1.18:5514' /etc/rsyslog.conf
+sed -i '/*.* @@10.15.1.18:5514/d' /etc/rsyslog.conf
