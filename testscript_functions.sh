9a58f017359c6a59a89243ca3b69521ec36d8b14
B.Kuiper <46485306+Badkuip@users.noreply.github.com>
2022-01-09 14:37:31 +0100

    Update testscript_functions.sh

--- testscript_functions.sh
+++ testscript_functions.sh
@@ -2,6 +2,8 @@
 service=''
 exitcode=''
 program=''
+string=''
+substring=''
 
 # function that checks exitcode for services, to test if service is running
 check_exitcode () {
@@ -19,6 +21,13 @@ check_program () {
   tput setaf 7
 }
 
+# check if substring in string/text
+check_string () {
+  if [[ ! "$string" == *"$substring"* ]]; then
+    tput setaf 1; echo "$substring is not in string"
+  fi
+  tput setaf 7
+}
 
 # example for checking if a service (nfs-server) is running
 service='nfs-server'
@@ -26,6 +35,11 @@ systemctl is-active --quiet $service
 exitcode=$(echo $?)
 check_exitcode
 
-# exampel for checking if program (firefox) is installed
+# example for checking if program (firefox) is installed
 program='firefox'
 check_program
+
+# example for checking if a substring is in a string
+string=$(exportfs)
+substring="/srv/ldap-home	192.168.20.0/24"
+check_string
