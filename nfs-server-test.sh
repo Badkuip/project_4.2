5dabb7a9db4cafaadac89bbad1fe6cbe6f1a3a10
B.Kuiper <46485306+Badkuip@users.noreply.github.com>
2022-01-09 16:09:45 +0100

    Update nfs-server-test.sh

--- nfs-server-test.sh
+++ nfs-server-test.sh
@@ -1,22 +1,90 @@
 #!/bin/bash
 service=''
 exitcode=''
+folder=''
+string=''
+substring=''
 
-# function that checks exit code
+# function that checks exitcode for services, to test if service is running
 check_exitcode () {
-  if [$exitcode = '1']; then
+  if [ ! $exitcode = '0' ]; then
     tput setaf 1; echo "$service not running"
-  if
+  fi
+  tput setaf 7
 }
 
-# check nfs is running
-exitcode=$(systemctl is-active --quiet nfs-server.service | echo $?)
-$service='nfs-server.service'
+# checking if nfs-server is running
+service='nfs-server'
+systemctl is-active --quiet $service
+exitcode=$(echo $?)
 check_exitcode
 
-# check nfs is running
-exitcode=$(systemctl is-active --quiet firewalld | echo $?)
-$service='firewall-cmd'
+# checking if nfs-server is running
+service='firewalld'
+systemctl is-active --quiet $service
+exitcode=$(echo $?)
 check_exitcode
 
-echo 'Test script has run.'
+
+
+# check if substring in string/text
+check_string () {
+  if [[ ! "$string" == *"$substring"* ]]; then
+    tput setaf 1; echo "$substring is not found"
+  fi
+  tput setaf 7
+}
+
+# checking if nsf folders are in exportfs
+string=$(exportfs)
+substring="/srv/ldap-home	10.15.1.0/24"
+check_string
+substring="/srv/nfs-share	10.15.1.0/24"
+
+# checking if firewall settings exists
+check_string
+string=$(firewall-cmd --list-service)
+substring="nfs"
+check_string
+substring="rpc-bind"
+check_string
+substring="mountd"
+
+
+# function that checks fi program is installed
+check_folder() {
+  if [ ! -d "$folder" ]; then
+    tput setaf 1; echo "$folder does not exist." >&2
+  fi
+  tput setaf 7
+}
+
+# check if nfs folders exist
+folder='/srv/ldap-home'
+check_folder
+folder='/srv/nfs-share'
+check_folder
+
+# check if permissions of nfs folders are correct
+string=$(stat -c "%A %U %G %N" /srv/ldap-home)
+substring="drwxrwxrwx nobody nobody '/srv/ldap-home'"
+check_string
+string=$(stat -c "%A %U %G %N" /srv/nfs-share)
+substring="drwxrwxrwx nobody nobody '/srv/nfs-share'"
+check_string
+
+
+
+# check network connection
+check_network () {
+  tput setaf 1 
+  ping -q -c1 8.8.8.8 &>/dev/null || echo 'The computer has no network connection'
+  tput setaf 7
+}
+check_network
+
+
+
+# script end
+tput setaf 2; echo 'Test script has run successfully.'
+tput setaf 7
